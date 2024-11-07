619f4d014b52f72edeffab45f092db571d49a7b4
KlevinMutombo <klevin.mutombo@gmail.com>
2024-11-07 11:55:47 -0500

    testcase 2005
    update code so username is echoed in clientconsole

--- edu/seg2105/client/backend/ChatClient.java
+++ edu/seg2105/client/backend/ChatClient.java
@@ -62,7 +62,7 @@ public class ChatClient extends AbstractClient
    */
   public void handleMessageFromServer(Object msg) 
   {
-    clientUI.display(msg.toString());
+    clientUI.display(loginId + "> "+ msg.toString());
     
     
   }
